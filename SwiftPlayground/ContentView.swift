e2a5434949e0fbe570843fca61b30c8a163bb2b8
Natthapong Intharak <natthapong.intharak@thoughtworks.com>
2021-02-01 17:01:14 +0700

    Initial Commit

---
+++ SwiftPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftPlayground
+//
+//  Created by Natthapong Intharak on 1/2/2564 BE.
+//  Copyright © 2564 ThoughtWorks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
